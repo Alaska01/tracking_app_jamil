ea38325aee9b568b681aa59eba1c0b80689a4d4d
Alaska01 <aadaniel108@gmail.com>
2022-12-03 15:25:48 +0100

    Add search ability by date range

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,7 +3,9 @@ class CustomersController < ApplicationController
 
   # GET /customers or /customers.json
   def index
-    @customers = Customer.all
+    @search = CustomerSearch.new(params[:search])
+    @customers = @search.scope
+    # @customers = Customer.all
   end
 
   # GET /customers/1 or /customers/1.json
---
+++ app/models/customer_search.rb
@@ -0,0 +1,22 @@
+class CustomerSearch
+  attr_reader :date_from, :date_to
+
+  def initialize(params)
+    params ||= {}
+    @date_from = parsed_date(params[:date_from], 3.days.ago.to_date.to_s)
+    @date_to = parsed_date(params[:date_to], Date.today.to_s)
+  end
+
+  def scope
+    Customer.where('date BETWEEN ? AND ?', @date_from, @date_to)
+  end
+
+    private
+      def parsed_date(date_string, default)
+        Date.parse(date_string)
+
+        rescue ArgumentError, TypeError
+          default
+      end
+
+end
