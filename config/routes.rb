61b03980de71fe420b096f67c046e88adaa4ece8
Alaska01 <aadaniel108@gmail.com>
2022-12-03 10:10:30 +0100

    Add bootsrap to buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :movements, only: [:show]
   resources :customers do
     # resources :movements, except: [:index],  controller: 'customers/movements'
     resources :movements, controller: 'customers/movements'
     
   end
 
-  # resources :movements, only: [:show]
-  # , only: [:index, :new, :update, :create, :edit], 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
